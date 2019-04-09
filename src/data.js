fadaff98af55562f3bcf0d739bbfca5701a2f17e
BELKHIRI <[lyessbelkhiri@gmail.com]>
2019-04-09 21:40:22 +0200

    rien

--- src/data.js
+++ src/data.js
@@ -17,6 +17,18 @@ export const storeProducts = [
     total: 0
   },
   {
+    id: "1",
+    title: "Cisco 2018 ",
+    img: "imgV/product-1.jpg",
+    price: 120,
+    company: "Cisco",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
     id: "2",
     title: "Montre connecté",
     img: "imgV/product-2.jpg",
